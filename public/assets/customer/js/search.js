c70cd0c831d2a75000c4cf4cc41e66d6ce3dcd6b
odeth2023 <odeth18@hotmail.es>
2023-12-19 03:31:18 -0500

    Feat[Search, Client Interface][v2.0]

--- public/assets/customer/js/search.js
+++ public/assets/customer/js/search.js
@@ -24,25 +24,29 @@ $(document).ready(function() {
 
   function displayAutocomplete(products) {
     autocompleteContainer.empty();
-
+  
     if (searchInput.val() === '') {
       return;
     }
-
-    products.forEach(product => {
-      const suggestion = $('<div></div>')
-        .addClass('autocomplete-suggestion border-bottom')
-        .html(`<div>
-                 
-                 ${product.name}
-               </div>
-               <div>${product.sale_price}</div>`)
-        .on('click', function() {
-          searchInput.val(product.name);
-          autocompleteContainer.empty();
-        });
-
-      autocompleteContainer.append(suggestion);
-    });
+  
+    // Verifica si products es un array antes de intentar iterar sobre él
+    if (Array.isArray(products)) {
+      products.forEach(product => {
+        const suggestion = $('<div></div>')
+          .addClass('autocomplete-suggestion border-bottom')
+          .html(`<div>${product.name}</div><div>${product.sale_price}</div>`)
+          .on('click', function() {
+            searchInput.val(product.name);
+            autocompleteContainer.empty();
+          });
+  
+        autocompleteContainer.append(suggestion);
+      });
+    } else if (products.message) {
+      console.log(products.message);
+    } else {
+      console.error('error:', products);
+    }
   }
+  
 });
