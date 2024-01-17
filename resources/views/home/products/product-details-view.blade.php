eb7ef5f79c2c1aa6c642e6ad5734616c150fac7e
IJanoco <janoco_13@hotmail.com>
2024-01-17 18:21:49 -0500

    Update [Product Details]

--- resources/views/home/products/product-details-view.blade.php
+++ resources/views/home/products/product-details-view.blade.php
@@ -40,28 +40,20 @@
                 
                 <div class="col-xl-6">
                     <div class="product__details-content">
-                        <h6>Samsung Galaxy A12, 32GB, Black – (Locked)</h6>
+                        <h6>{{ $productDetails->name }}</h6>
                         <div class="pd-rating mb-10">
                             <ul class="rating">
-                                <li><a href="#"><i class="fal fa-star"></i></a></li>
-                                <li><a href="#"><i class="fal fa-star"></i></a></li>
-                                <li><a href="#"><i class="fal fa-star"></i></a></li>
-                                <li><a href="#"><i class="fal fa-star"></i></a></li>
-                                <li><a href="#"><i class="fal fa-star"></i></a></li>
+                                <li><a href="#"><i class="fas fa-star"></i></a></li>
+                                <li><a href="#"><i class="fas fa-star"></i></a></li>
+                                <li><a href="#"><i class="fas fa-star"></i></a></li>
+                                <li><a href="#"><i class="fas fa-star"></i></a></li>
+                                <li><a href="#"><i class="fas fa-star"></i></a></li>
                             </ul>
                             <span>(01 review)</span>
                             <span><a href="#">Add your review</a></span>
                         </div>
                         <div class="price mb-10">
-                            <span>$216.00</span>
-                        </div>
-                        <div class="features-des mb-20 mt-10">
-                            <ul>
-                                <li><a href="product-details.html"><i class="fas fa-circle"></i> Bass and Stereo Sound.</a></li>
-                                <li><a href="product-details.html"><i class="fas fa-circle"></i> Display with 3088 x 1440 pixels resolution.</a></li>
-                                <li><a href="product-details.html"><i class="fas fa-circle"></i> Memory, Storage &amp; SIM: 12GB RAM, 256GB.</a></li>
-                                <li><a href="product-details.html"><i class="fas fa-circle"></i> Androi v10.0 Operating system.</a></li>
-                            </ul>
+                            <span>S/ {{ $productDetails->sale_price }}</span>
                         </div>
                         <div class="product-stock mb-20">
                             <h5>Availability: <span> 940 in stock</span></h5>
@@ -75,34 +67,22 @@
                         <div class="details-meta">
                             <div class="d-meta-left">
                                 <div class="dm-item mr-20">
-                                    <a href="#"><i class="fal fa-heart"></i>Add to wishlist</a>
+                                    <a href="#"><i class="fas fa-heart"></i>Add to wishlist</a>
                                 </div>
-                                <div class="dm-item">
-                                    <a href="#"><i class="fal fa-layer-group"></i>Compare</a>
-                                </div>
-                            </div>
-                            <div class="d-meta-left">
-                                <div class="dm-item">
-                                    <a href="#"><i class="fal fa-share-alt"></i>Share</a>
-                                </div>
+                                
                             </div>
                         </div>
                         <div class="product-tag-area mt-15">
                             <div class="product_info">
-                                <span class="sku_wrapper">
-                                    <span class="title">SKU:</span>
-                                    <span class="sku">DK1002</span>
-                                </span>
-                                <span class="posted_in">
-                                    <span class="title">Categories:</span>
-                                    <a href="#">iPhone</a>
-                                    <a href="#">Tablets</a>
-                                </span>
+                                
                                 <span class="tagged_as">
                                     <span class="title">Tags:</span>
-                                    <a href="#">Smartphone</a>, 
-                                    <a href="#">Tablets</a>
+                                    @foreach ($productDetails->labels as $index => $label)
+                                        {{ strtoupper($label->name) }}@if($index < count($productDetails->labels) - 1), @endif
+                                    @endforeach                  
+                                    .
                                 </span>
+                                
                             </div>
                         </div>
                     </div>
@@ -122,12 +102,12 @@
                             <li class="nav-item" role="presentation">
                               <button class="nav-link active" id="des-tab" data-bs-toggle="tab" data-bs-target="#des" type="button" role="tab" aria-controls="des" aria-selected="true">Description </button>
                             </li>
-                            <li class="nav-item" role="presentation">
+                           {{--  <li class="nav-item" role="presentation">
                                 <button class="nav-link" id="aditional-tab" data-bs-toggle="tab" data-bs-target="#aditional" type="button" role="tab" aria-controls="aditional" aria-selected="false">Additional information</button>
                               </li>
                             <li class="nav-item" role="presentation">
                               <button class="nav-link" id="review-tab" data-bs-toggle="tab" data-bs-target="#review" type="button" role="tab" aria-controls="review" aria-selected="false">Reviews (1) </button>
-                            </li>
+                            </li> --}}
                           </ul>
                     </div>
                 </div>
@@ -135,58 +115,12 @@
             <div class="tab-content" id="prodductDesTaContent">
                 <div class="tab-pane fade active show" id="des" role="tabpanel" aria-labelledby="des-tab">
                     <div class="product__details-des-wrapper">
-                        <p class="des-text mb-35">Designed by Hans J. Wegner in 1949 as one of the first models created especially for Carl Hansen & Son, and produced since 1950. The last of a series of chairs wegner designed based on inspiration from antique Chinese armchairs. The gently rounded top together with the back and seat offers a variety of comfortable seating positions,ideal for both long visits to the dining table and relaxed lounging.</p>
-                        <h6 class="des-sm-title">The standard passage, used since the 1500s.</h6>
-                        <p class="des-text mb-35">A light chair, easy to move around the dining table and about the room. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                        <div class="features-des-image text-center">
-                            <img src="assets/img/features-product/product-content-1.jpg" alt="">
-                        </div>
-                        <div class="product-des-section mb-90">
-                            <h5 class="des-section mb-30">Get 30% Daily Cash
-                              <br>  Back with Membership Card.</h5>
-                              <p>A new collection of lounge furniture, occasional tables and a stool by Edward Barber & Jay Osgerby offers a relaxed, contemporary attitude toward interior design. The lounge furniture includes four individualized sized sofas, and three complementary ottomans. Available in a range of upholstery fabrics and leathers, the lounge furniture is distinguished by stitched seams that reinforce its architectural profile, softened by the curvature of cushions on each face range of upholstery fabrics and leathers.</p>
-                        </div>
-                        <div class="row mb-80">
-                            <div class="col-xl-6">
-                                <div class="des-single mb-30 text-center">
-                                    <div class="features-des-image text-center">
-                                        <img src="assets/img/features-product/product-content-2.jpg" alt="">
-                                    </div>
-                                    <h5 class="des-section">Get 30% Daily Cash
-                                        <br>  Back with Membership Card.</h5>
-                                    <p>Sit amet conse ctetur adipisicing elit, sed do <br> eiusmod tempor incididunt ut labore et dolore  magna aliqua.</p>
-                                </div>
-                            </div>
-                            <div class="col-xl-6">
-                                <div class="des-single text-center mb-30">
-                                    <div class="features-des-image">
-                                        <img src="assets/img/features-product/product-content-3.jpg" alt="">
-                                    </div>
-                                    <h5 class="des-section text-center">Get 70% Daily Cash
-                                        <br>  Back with Membership Card.</h5>
-                                    <p>Sit amet conse ctetur adipisicing elit, sed do <br> eiusmod tempor incididunt ut labore et dolore  magna aliqua.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="features-des-image features-des-image-2 text-center mb-50 w-img">
-                            <img src="assets/img/features-product/product-content-4.jpg" alt="">
-                        </div>
-                        <div class="des-sm-features">
-                            <div class="des-sm-fet text-center mb-30">
-                                <img src="assets/img/features-product/product-content-5.png" alt="">
-                                <span>Ultra Wide Angle</span>
-                            </div>
-                            <div class="des-sm-fet text-center mb-30">
-                                <img src="assets/img/features-product/product-content-6.png" alt="">
-                                <span>Live Focus On</span>
-                            </div>
-                        </div>
-                        <p class="des-text mb-35">Designed by Puik in 1949 as one of the first models created especially for Carl Hansen & Son, and produced since 1950. The last of a series of chairs wegner designed based on inspiration from antique Chinese armchairs. The gently rounded top together with the back and seat offers a variety of comfortable seating positions,ideal for both long visits to the dining table and relaxed lounging. A light chair easy to move around the dining table and about the room. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla.</p>
-                        <h6 class="des-sm-title">Section 1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC.</h6>
-                        <p class="des-text mb-25">Sound of Marshall, unplugs the chords, and takes the show on the road. Weighing in under 7 pounds, the Kilburn is a lightweight piece of vintage styled engineering. Setting the bar as one of the loudest speakers in its class, the Kilburn is a compact, stout-hearted hero with a well-balanced audio which boasts a clear midrange and extended highs for a sound that is both articulate and pronounced. The analogue knobs allow you to fine tune the controls to your personal preferences while the guitar-influenced leather strap enables easy and stylish travel.</p>
+                        
+                        {{ $productDetails->description }}
+
                     </div>
                 </div>
-                <div class="tab-pane fade" id="aditional" role="tabpanel" aria-labelledby="aditional-tab">
+              {{--   <div class="tab-pane fade" id="aditional" role="tabpanel" aria-labelledby="aditional-tab">
                     <div class="product__desc-info">
                         <ul>
                            <li>
@@ -328,7 +262,7 @@
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> --}}
             </div>
         </div>
     </div>
